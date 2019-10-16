14f9590971b50d59242adda07db269b90c02e8b3
moshe <moshe.v@position-in.com>
2019-10-16 18:39:01 +0300

    changed the flow order

--- my_sub_project/main_celery/eval_coll_flow.py
+++ my_sub_project/main_celery/eval_coll_flow.py
@@ -3,7 +3,7 @@ from my_sub_project.collection.main_collection import main_coll_task
 
 
 def collect_and_eval():
-    chain = main_evaluation_task.s() | main_coll_task.s()
+    chain = main_coll_task.s() | main_evaluation_task.s()
     chain()
 
 
